0603533cedac76d13ac9be796c02fcb8225428b1
plumccc <2253206517@qq.com>
2021-09-03 16:12:18 +0800

    fix 25.k-个一组翻转链表

--- 25.k-个一组翻转链表.js
+++ 25.k-个一组翻转链表.js
@@ -20,34 +20,27 @@
 var reverseKGroup = function (head, k) {
 
   /** 链表 --> 数组 --> 反转 --> 生成新链表 */
-
-  // let headArr = [];
-  // while (head) {
-  //   headArr.push(head.val)
-  //   head = head.next
-  // }
-
-  // let list = new ListNode()
-  // let cur = list
-  // let i = 0
-  // while (i < headArr.length) {
-  //   const curArr = headArr.slice(i, i + k)
-  //   if (i + k <= headArr.length) {
-  //     curArr.reverse()
-  //   }
-  //   curArr.forEach(el => {
-  //     cur.next = new ListNode(el)
-  //     cur = cur.next
-  //   })
-  //   i += k
-  // }
-  // return list.next
-
-  /** */
+  let headArr = [];
   while (head) {
-    console.log(head.next)
+    headArr.push(head.val)
     head = head.next
   }
+
+  let list = new ListNode()
+  let cur = list
+  let i = 0
+  while (i < headArr.length) {
+    const curArr = headArr.slice(i, i + k)
+    if (i + k <= headArr.length) {
+      curArr.reverse()
+    }
+    curArr.forEach(el => {
+      cur.next = new ListNode(el)
+      cur = cur.next
+    })
+    i += k
+  }
+  return list.next
 };
 // @lc code=end
 
