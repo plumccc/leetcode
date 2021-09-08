ddf21911c0a662e8467e361c08a1a7d8334e643c
wangxingyu <2253206517@qq.com>
2021-09-08 15:54:20 +0800

    finish 33.搜索旋转排序数组.js

---
+++ 33.搜索旋转排序数组.js
@@ -0,0 +1,18 @@
+/*
+ * @lc app=leetcode.cn id=33 lang=javascript
+ *
+ * [33] 搜索旋转排序数组
+ */
+
+// @lc code=start
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number}
+ */
+var search = function(nums, target) {
+  // 原生 api
+  return nums.indexOf(target)
+};
+// @lc code=end
+
