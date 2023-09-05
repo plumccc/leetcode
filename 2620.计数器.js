779ffedef3089f3719474cd8c21f58b910ff8228
wangxingyu <2253206517@qq.com>
2023-09-05 10:05:13 +0800

    feat: 2023-09-02

---
+++ 2620.计数器.js
@@ -0,0 +1,24 @@
+/*
+ * @lc app=leetcode.cn id=2620 lang=javascript
+ *
+ * [2620] 计数器
+ * 
+ * https://leetcode.cn/problems/counter/
+ */
+// @lc code=start
+/**
+ * @param {number} n
+ * @return {Function} counter
+ */
+var createCounter = function (n) {
+  return function () {
+    return n++;
+  };
+};
+
+/** 
+* const counter = createCounter(10)
+* counter() // 10
+* counter() // 11
+* counter() // 12
+*/
