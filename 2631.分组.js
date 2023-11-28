c491b1cbb917b5480b32027c51ca1ce12ba5322e
wangxingyu <2253206517@qq.com>
2023-11-28 17:40:53 +0800

    feat: 2023-11-28

---
+++ 2631.分组.js
@@ -0,0 +1,27 @@
+/*
+ * @lc app=leetcode.cn id=2631 lang=javascript
+ *
+ * [2631] 计数器
+ * 
+ * https://leetcode.cn/problems/group-by/description/
+ */
+// @lc code=start
+/**
+ * @param {Function} fn
+ * @return {Object}
+ */
+Array.prototype.groupBy = function (fn) {
+  return this.reduce((prev, cur) => {
+    const key = fn(cur);
+    if (key in prev) {
+      prev[key].push(cur)
+    } else {
+      prev[key] = [cur];
+    }
+    return prev;
+  }, {})
+};
+
+/**
+ * [1,2,3].groupBy(String) // {"1":[1],"2":[2],"3":[3]}
+ */
---
+++ 2667.创建 Hello World 函数.js
@@ -0,0 +1,21 @@
+/*
+ * @lc app=leetcode.cn id=2667 lang=javascript
+ *
+ * [2667] 创建 Hello World 函数
+ * 
+ * https://leetcode.cn/problems/create-hello-world-function/description/
+ */
+// @lc code=start
+/**
+ * @return {Function}
+ */
+var createHelloWorld = function () {
+  return function (...args) {
+    return "Hello World";
+  }
+};
+
+/**
+ * const f = createHelloWorld();
+ * f(); // "Hello World"
+ */
