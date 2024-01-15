100201ff55525eb34bce0eb96e1344431e52ee22
wangxingyu <2253206517@qq.com>
2024-01-15 18:29:50 +0800

    feat: 2024-01-15

---
+++ 905.按奇偶排序数组.js
@@ -0,0 +1,26 @@
+/*
+ * @lc app=leetcode.cn id=905 lang=javascript
+ *
+ * [905] 按奇偶排序数组
+ * 
+ * https://leetcode.cn/problems/sort-array-by-parity/
+ */
+
+/**
+ * @param {number[]} nums
+ * @return {number[]}
+ */
+var sortArrayByParity = function (nums) {
+  const res = [];
+
+  for (let i = 0; i < nums.length; i++) {
+    if (nums[i] % 2) {
+      res.push(nums[i])
+    } else {
+      res.unshift(nums[i])
+    }
+  }
+
+  return res;
+};
+
